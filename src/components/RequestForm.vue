e22f71e3619a68b0052c10aebe34c984b03dce82
AnnaNX1 <achervonyuk@usetech.com>
2023-02-26 12:44:17 +0300

    add forms

---
+++ src/components/RequestForm.vue
@@ -0,0 +1,60 @@
+<script lang="ts" setup>
+defineProps<{
+  title: { type: string, required: true },
+  runButtonTitle: { type: string, required: true },
+  filesDescription: {
+    type: [
+      {
+        name: string,
+        description: string,
+        link: string
+      }],
+    required: true
+  }
+}
+>()
+</script>
+
+<template>
+      <h1 class="text-h2 font-weight-bold">{{title}}</h1>
+
+      <div class="py-14" />
+
+      <v-row class="d-flex align-center justify-center">
+        <v-col cols="auto">
+          <h2>Load files:</h2>
+          <div v-for="file in filesDescription">
+            {{file.name}}
+            <span v-if="file.description">({{file.description}})</span>
+            <span v-if="file.link">load</span>
+          </div>
+        </v-col>
+
+        <v-col cols="auto">
+          <v-file-input
+            multiple
+            label="File input"
+          ></v-file-input>
+        </v-col>
+
+        <v-col cols="auto">
+          <v-btn
+            color="primary"
+            href="https://next.vuetifyjs.com/introduction/why-vuetify/#feature-guides"
+            min-width="228"
+            rel="noopener noreferrer"
+            size="x-large"
+            target="_blank"
+            variant="flat"
+          >
+            <v-icon
+              icon="mdi-speedometer"
+              size="large"
+              start
+            />
+
+            {{runButtonTitle}}
+          </v-btn>
+        </v-col>
+      </v-row>
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,46 @@
 <template>
-  <HelloWorld />
+  <v-container>
+    <v-responsive class="d-flex align-center text-center fill-height">
+      <h1 class="text-h2 font-weight-bold">YM</h1>
+      <request-form
+        title="get eff"
+        runButtonTitle="eff"
+        :filesDescription="[
+          {name: 'price', description: 'price descr'},
+          {name: 'parse YM', description: 'parse YM descr'}
+        ]"
+      ></request-form>
+      <request-form
+        title="get price"
+        runButtonTitle="price"
+        :filesDescription="[
+          {name: 'price', description: 'price descr'},
+          {name: 'parse YM', description: 'parse YM descr'}
+        ]"
+      ></request-form>
+      <h1 class="text-h2 font-weight-bold">Ozon</h1>
+      <request-form
+        title="get eff"
+        runButtonTitle="eff"
+        :filesDescription="[
+          {name: 'price', description: 'price descr'},
+          {name: 'parse YM', description: 'parse YM descr'}
+        ]"
+      ></request-form>
+      <request-form
+        title="get price"
+        runButtonTitle="price"
+        :filesDescription="[
+          {name: 'price', description: 'price descr'},
+          {name: 'parse YM', description: 'parse YM descr'}
+        ]"
+      ></request-form>
+    <!--  <HelloWorld />-->
+    </v-responsive>
+  </v-container>
 </template>
 
 <script lang="ts" setup>
   import HelloWorld from '@/components/HelloWorld.vue'
+  import RequestForm from "@/components/RequestForm.vue"
 </script>
